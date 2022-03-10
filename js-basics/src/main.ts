252af2f546ff15698220d2c8f32db6fee9cc0816
YBRua <ybiur0929@163.com>
2022-03-10 12:57:51 +0800

    [FEATURE] Added endgame modal w/ score display

--- js-basics/src/main.ts
+++ js-basics/src/main.ts
@@ -5,6 +5,24 @@ import { setScore } from "./gameplay/scoreboard";
 import { genMap } from "./mapGeneration";
 import { drawPlayer, initMapRendering } from "./mapRender";
 
+const endgameModal = document.getElementById("modal-endgame")!;
+const modalBG = document.getElementById("modal-container")!;
+
+function showEndgameModal() {
+  modalBG.classList.remove("hidden");
+  endgameModal.classList.remove("hidden");
+}
+
+function hideEndgameModal() {
+  modalBG.classList.add("hidden");
+  endgameModal.classList.add("hidden");
+}
+
+function endgameBtnOnClickHandler() {
+  hideEndgameModal();
+  refreshGame();
+}
+
 export function refreshGame() {
   // generate and render a new map
   const map = genMap(SIZE, NUM_BONUS, SCORE_PER_CELL);
@@ -21,3 +39,4 @@ export function refreshGame() {
 
 document.getElementById("btn-start-game")!.onclick = refreshGame;
 document.addEventListener("keydown", keydownEvtHandler);
+document.getElementById('endgame-btn')!.onclick = endgameBtnOnClickHandler;
