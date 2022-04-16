8495373773e1717fcb959b7ad1e0aa328f234f94
YBRua <YBiuR0929@163.com>
2022-04-16 14:45:04 +0800

    Updated title icon

--- performance-optim/src/components/Title.tsx
+++ performance-optim/src/components/Title.tsx
@@ -1,14 +1,25 @@
 import "./styles/Title.scss";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function Title() {
   return (
     <header className="title-container">
-      <h1 className="main-title">
-        🍪 Unofficial Oreo Compiler 🍪
-      </h1>
-      <h2 className="main-subtitle">
-        An SJTU CS176 Course Assignment
-      </h2>
+      <div className="main-title-container">
+        <LazyLoadImage
+          src="./src/figs/minified/oreo.png"
+          alt="I'm Oreo"
+          width={50}
+          height={50}
+        ></LazyLoadImage>
+        <h1 className="main-title">Unofficial Oreo Compiler</h1>
+        <LazyLoadImage
+          src="./src/figs/minified/oreo.png"
+          alt="I'm Oreo"
+          width={50}
+          height={50}
+        ></LazyLoadImage>
+      </div>
+      <h2 className="main-subtitle">An SJTU CS176 Course Assignment</h2>
     </header>
   );
 }
