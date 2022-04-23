af82d86ffdb5af6250d222fa117e52fa41087f9c
YBRua <YBiuR0929@163.com>
2022-04-23 17:09:32 +0800

    Final project init

---
+++ final-project-game/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    Hello There.
+  </div>;
+}
+
+export default App;
